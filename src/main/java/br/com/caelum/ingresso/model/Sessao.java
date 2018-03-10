b4d955cb6d44b8b3599bfa388131f8bcdf864d08
empessoa <empessoa8@gmail.com>
2018-03-10 14:00:48 -0300

    Implemtentado metodo de sessao

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,11 +2,29 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sessao {
+	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	private LocalTime horario;
+	@ManyToOne
 	private Sala sala;
+	@ManyToOne
 	private Filme filme;
+	
+	/*
+	 * @deprecated hibernate only
+	*/
+	public Sessao(){
+		
+	}
 	
 	public Sessao(LocalTime horario, Filme filme, Sala sala){
 		this.horario = horario;
@@ -14,7 +32,7 @@ public class Sessao {
 		this.sala = sala;
 	}
 	
-	public LocalTime getHorariTermino(){
+	public LocalTime getHorarioTermino(){
 		return this.horario.plusMinutes(filme.getDuracao().toMinutes());
 	}
 
